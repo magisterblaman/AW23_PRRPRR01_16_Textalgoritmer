c7f535f8b7ce65781407b7f6952458db5f2ea739
magisterblaman <hannes.blaman@lbs.se>
2025-01-30 12:22:22 +0100

    Kryptering

--- AW23_PRRPRR01_16_Textalgoritmer/Program.cs
+++ AW23_PRRPRR01_16_Textalgoritmer/Program.cs
@@ -11,36 +11,57 @@ namespace AW23_PRRPRR01_16_Textalgoritmer {
 			return char.ToUpper(input) - 'A' + 1;
 		}
 
+		static string EncryptString(string input, int shift) {
+			string output = "";
+			for (int i = 0; i < input.Length; i++) {
+				output += (char)(input[i] + shift);
+			}
+			return output;
+		}
+
+		static string DecryptString(string input, int shift) {
+			return EncryptString(input, -shift);
+		}
+
+		static string alphabet = "abcdefghijklmnopqrstuvwxyzåäö";
+
+		static int FindPositionInAlphabet(char input) {
+			input = char.ToLower(input);
+			for (int i = 0; i < alphabet.Length; i++) {
+				if (alphabet[i] == input) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		static string BetterEncrypt(string input, int shift) {
+			string output = "";
+			for (int i = 0; i < input.Length; i++) {
+				int inputLetterPos = FindPositionInAlphabet(input[i]);
+				int newLetterPos = inputLetterPos + shift;
+				int modulo = BetterModulus(newLetterPos, alphabet.Length);
+				output += alphabet[modulo];
+			}
+			return output;
+		}
+
+		static string BetterDecrypt(string input, int shift) {
+			return BetterEncrypt(input, -shift);
+		}
+
+		static int BetterModulus(int num, int den) {
+			return (num % den + den) % den;
+		}
+
 		static void Main(string[] args) {
 
-			//string myText = "  Hej jag heter Hannes tjena tjena     ";
+			string myInput = "älgzoo123";
+			string myOutput = BetterEncrypt(myInput, 5);
+			//string decrypted = BetterDecrypt(myInput, 5);
 
-			//string trimmed = myText.Trim();
-			//Console.WriteLine("|" + trimmed + "|");
-
-			//string uppercase = myText.ToUpper();
-			//string lowercase = myText.ToLower();
-
-			//if (myText.Length > 35) {
-			//	string extracted = myText.Substring(25, 10);
-
-			//	Console.WriteLine(extracted);
-			//}
-
-			//string text2 = "Hello";
-			//char[] text2AsCharArray = new char[] { 'H', 'e', 'l', 'l', 'o' };
-
-			//char[] converted = text2.ToCharArray();
-			//converted[2] = 'n';
-			//string convertedBack = new string(converted);
-
-			//for (int i = 0; i < convertedBack.Length; i++) {
-			//	Console.Write(convertedBack[i]);
-			//}
-
-			//Console.WriteLine((char)('L' + 5));
-
-			Console.WriteLine(GetNumberInAlphabet('å'));
+			Console.WriteLine(myOutput);
+			//Console.WriteLine(decrypted);
 
 		}
 	}
